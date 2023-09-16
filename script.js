62c4665dbd0f5bfad72fd620ae416ef6b4c9bff7
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-16 11:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const btn = document.getElementById('enterBtn')
 btn.addEventListener('click',()=>{
 	let newText = document.createElement("h1")
 	newText.textContent = "Entered Metaverse"
+	newText.setAttribute('id','status');
 	text.parentNode.replaceChild(newText, text)
 })
